2143b9c9e44b180dcac8ca04c30f17dd78f47c76
jacobdykes405 <jacobdykes405@gmail.com>
2023-01-22 14:06:18 -0500

    great progress have game playing five rounds

--- Rock Paper Scissors/javascript.js
+++ Rock Paper Scissors/javascript.js
@@ -1,21 +1,47 @@
+// Constants for the game //
+
 const choices = ['rock','paper','scissors']
-
-function game(){
-
-}
-
-function playRound(){
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
-
-}
+ 
+// Prompts User for rock paper or scissors //
 
 function playerChoice () {
-let input = prompt("Rock, Paper, Scissors ?")
-input = input.toLowerCase();
-return input
+let myChoice = prompt("Rock, Paper, Scissors ?")
+myChoice = myChoice.toLowerCase();
+return myChoice
 }
+
+// Generates a random answer //
 
 function computerChoice(){
     return choices[Math.floor(Math.random()*choices.length)]
 }
+
+// plays a single round of Rock Paper Scissors
+
+function playRound(spot1, spot2){
+    if (spot1 == "rock" && spot2 == "paper") {
+        console.log('Loose, Paper beats rock!');
+    }else if (spot1 == "rock" && spot2== "scissors") {
+        console.log('Win, Rock beats Scissors!');
+    }else if (spot1 == "paper" && spot2== "scissors") {
+        console.log('Loose, Scissor beats paper!');
+    }else if (spot1 == "paper" && spot2 == "rock") {
+        console.log("Win, Paper beats rock!");
+    }else if (spot1 == "scissors" && spot2 == "rock"){
+        console.log("Loose, Rock beats Scissors!");
+    } else if (spot1 == 'scissors' && spot2 === 'paper'){
+        console.log('Win, Scissors beats paper!');
+    }else if (spot1 == spot2){
+        console.log("tie");
+    }
+}
+//plays five rounds of rock paper scissors
+
+function playGame(){
+    for (i=1; i<=5; i++) {
+        playRound(playerChoice(), computerChoice());
+    }
+}
+
+playGame()
+   
